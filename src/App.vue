f0d2fb99dea5253fb470d2606faf0776ef47ab9c
Takedownpath <Takedownpath@gmail.com>
2024-10-14 20:36:38 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -104,7 +104,7 @@ export default {
           "callsign": "Asylum",
           "alias": "Weiss Alison",
           "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
-          "corpro": "Horus",
+          "corpro": "HORUS",
           "frame": "Balor",
           "mech": "snek"
         },
